254fa6aeefb943b428d71892fe1ffa7bc73351c8
gomezd <david.garcia.mx@gmail.com>
2013-08-29 23:16:46 +0000

    Added copyright notice

--- dimension.js
+++ dimension.js
@@ -1,4 +1,9 @@
 #! /usr/bin/env node
+/*
+ * Copyright (c) 2013, Yahoo! Inc. All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
 
 "use strict";
 
